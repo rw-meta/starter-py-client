fb522bfb7d0554c50c20008bf35d26e9828e6467
arturgspb <arturgspb@gmail.com>
2016-07-19 01:18:09 +0300

    publish fix

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 
 import setuptools
+from os import path
 
 from starter_api import info
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name=info.__package_name__,
     version=info.__version__,
+
+    description='Client for Starter Server API',
+    long_description=long_description,
+
+    url='https://github.com/rw-meta/starter-py-client',
+
     author='Artur Geraschenko',
     author_email='arturgspb@gmail.com',
-    description='Client for Starter Server API',
-    description_file='README.rst',
+
     license='MIT',
-    url='https://github.com/rw-meta/starter-py-client',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
     setup_requires=['requests'],
     packages=["starter_api"],
     package_data={'': ['LICENSE', 'NOTICE']},
